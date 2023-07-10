63d76c1421fa25fe7fcba57c6534d3dcbe4ae099
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-07-10 18:23:04 +0300

    Create post_controller.ex

---
+++ lib/blogging_platform_web/lib/blogging_platform_web/controllers/post_controller.ex
@@ -0,0 +1,34 @@
+defmodule Blog.PostController do
+  use Blog.Web, :controller
+
+  alias Blog.Post
+
+  def index(conn, _params) do
+    posts = Post.list_posts()
+    render(conn, "index.html", posts: posts)
+  end
+
+  def new(conn, _params) do
+    changeset = Post.changeset(%Post{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"post" => post_params}) do
+    changeset = Post.changeset(%Post{}, post_params)
+
+    case Post.create_post(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post created successfully.")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Post.get_post!(id)
+    render(conn, "show.html", post: post)
+  end
+end
